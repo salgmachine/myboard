ee878dc12de0686bc697f25c3a13b5d8d35d76da
salgmachine <salgmachine@bcfh.me>
2016-04-18 00:05:21 +0200

    updated project structure
    changed package structure

--- myboard/src/main/java/me/bcfh/myboard/repo/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package me.bcfh.myboard.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import me.bcfh.myboard.model.Item;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-
-}
